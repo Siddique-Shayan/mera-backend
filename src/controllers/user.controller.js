0ddb1dc5a273f89a61bbc3e0323c255b4ebeca29
Siddique-Shayan <shayanmohd463@gmail.com>
2025-08-11 08:08:54 +0530

    written og code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,14 +16,14 @@ const registerUser = asyncHandler(async (req,res) => {
     //return res
 
     const {fullName , email , username , password} = req.body
-    console.log("email: " , email);
+    // console.log("email: " , email);
 
     if([fullName,email,username,password].some((field) => field?.trim() === "")){
         throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser = User.findOne({
-        $or: [{username} , {email}]
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
 
     if(existedUser){
@@ -31,14 +31,20 @@ const registerUser = asyncHandler(async (req,res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required.")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = uploadOnCloudinary(coverImageLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
     
     if(!avatar){
         throw new ApiError(400,"Avatar file is required.")
@@ -58,7 +64,7 @@ const registerUser = asyncHandler(async (req,res) => {
     )
 
     if(!createdUser){
-        throw new ApiError(500 , "Something went wrong while registering a user")
+        throw new ApiError(500 , "Something went wrong while registering the user")
     }
 
     return res.status(201).json(
--- src/index.js
+++ src/index.js
@@ -1,61 +1,18 @@
-import dotenv from "dotenv";
-import { app } from "./app.js";
-dotenv.config();
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+
+
 
 connectDB()
 .then(() => {
-    app.on("err" , (err) => {
-        console.log('Error:' , err);
-        throw err;
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
-    app.listen(process.env.PORT || 8000 , () => {
-        console.log(`Server is running at port : ${process.env.PORT || 8000}`)
-    });
 })
 .catch((err) => {
-    console.log('MongoDB connection failed');
+    console.log("MONGO db connection failed !!! ", err);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import mongoose from "mongoose";
-// import {DB_NAME} from './constants'
-/*
-import express from "express";
-const app = express();
-
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("error" ,(error)=>{
-            console.log("ERR: " ,error);
-            throw error
-        })
-
-        app.listen(process.env.PORT , () => {
-            console.log(`App is listeniing on ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.error("ERROR: ",error);
-        throw error;
-    }
-})()
-
-*/
